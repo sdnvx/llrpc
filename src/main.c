fe9e40305f31296a47d244ddccc6d1481ca7fc88
Dmitry Sednev <dmitry@sednev.ru>
2025-11-17 06:57:54 +0300

    Initialize repository

---
+++ src/main.c
@@ -0,0 +1,12 @@
+//
+// LLRPC: Link-level RPC router
+// Copyright (c) 2025, Dmitry Sednev <dmitry@sednev.ru>
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <stdlib.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
